0c65d688eae89697155c651802b2108f7e60d394
nhanlnt <nhanlnt@hotmail.com>
2022-11-13 23:57:20 +0700

    feat: compress large images

--- src/components/banner/configs.js
+++ src/components/banner/configs.js
@@ -1,17 +1,7 @@
-import Image1 from "../../assets/images/image-1.gif";
-import Image2 from "../../assets/images/image-2.gif";
 import Image3 from "../../assets/images/image-3.png";
 import Image4 from "../../assets/images/image-4.jpg";
 import Image5 from "../../assets/images/image-5.jpg";
 import Image6 from "../../assets/images/image-6.png";
 import Image7 from "../../assets/images/image-7.png";
 
-export const bannerImages = [
-  Image1,
-  Image2,
-  Image3,
-  Image4,
-  Image5,
-  Image6,
-  Image7,
-];
+export const bannerImages = [Image3, Image4, Image5, Image6, Image7];
